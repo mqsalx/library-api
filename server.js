f9c7c4452623390298d5e67bc9b73655673e6fba
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-08 18:37:58 -0300

    Updated
    Refactoring...

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-import http from "http"
+import app from "./src/app.js"
 
 
 const PORT = 3000
 
-const routes = {
-    "/": "Node Course",
-    "/book": "Entered Book",
-    "/author": "Entered Author"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end(routes[req.url])
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
