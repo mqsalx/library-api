f69eab1a24b8fb6c4903931ce2db90eace848aa7
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-07-08 19:42:46 -0300

    Updated
    Improving the code with CRUD operations.

--- src/app.js
+++ src/app.js
@@ -10,21 +10,4 @@ connection.on("error", (error) => {
 const app = express()
 routes(app)
 
-app.get("/books/:id", (req, res)=> {
-    const index = getBookById(req.params.id)
-    res.status(200).json(books[index])
-})
-
-app.put("/books/:id", (req, res) => {
-    const index = getBookById(req.params.id)
-    books[index].title = req.body.title
-    res.status(200).json(books)
-})
-
-app.delete("/books/:id", (req, res) => {
-    const index = getBookById(req.params.id)
-    books.splice(index, 1)
-    res.status(200).send("Book deleted")
-})
-
 export default app
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -12,7 +12,7 @@ class BookController {
         }
     }
 
-    static async getBookById(req, res) {
+    static async getBook(req, res) {
         try {
             const id = req.params.id
             const retrieveBook = await books.findById(id)
@@ -23,12 +23,33 @@ class BookController {
         }
     }
 
+
     static async postBook(req, res) {
         try{
             const createBook = await books.create(req.body)
             res.status(201).json({message: "Book added", book: createBook})
         } catch (error) {
             res.status(500).json({message: `${error.message} - Error at adding book`})
+        }
+    }
+
+    static async putBook(req, res) {
+        try {
+            const id = req.params.id
+            const updateBook = await books.findByIdAndUpdate(id, req.body)
+            res.status(200).json({ message: "Book updated", book: updateBook })
+        } catch(error) {
+            res.status(500).json({message: `${error.message} - Error at updating book`})
+        }
+    }
+
+    static async deleteBook(req, res) {
+        try {
+            const id = req.params.id
+            const removeBook = await books.findByIdAndDelete(id)
+            res.status(200).json({ message: "Book deleted"})
+        } catch(error) {
+            res.status(500).json({message: `${error.message} - Error at deleting book`})
         }
     }
 }
